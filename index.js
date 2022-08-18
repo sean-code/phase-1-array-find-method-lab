e38a6cf50d4db67c1dfab297762d414ba38a1e40
sean-code <john.nganga@student.moringaschool.com>
2022-08-18 18:44:34 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+superbowlWin = (record) => {
+    const result = record.find( record => record.result === "W" );
+// Ternary Expression
+    return !!result ? result.year : undefined;
+}
+
+
+
